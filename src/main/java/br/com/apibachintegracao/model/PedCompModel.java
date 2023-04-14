62f3a883f885651c9465d071a9b3a8909e62c65d
BRQ\dbosco <dbosco@brq.com>
2023-04-14 18:50:14 -0300

    ajustando pom

--- src/main/java/br/com/apibachintegracao/model/PedCompModel.java
+++ src/main/java/br/com/apibachintegracao/model/PedCompModel.java
@@ -5,12 +5,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
