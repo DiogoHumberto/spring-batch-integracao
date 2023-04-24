37cb7edb2dc7c1ed78c3176497cfe79882263ade
BRQ\dbosco <dbosco@brq.com>
2023-04-24 18:51:23 -0300

    Add exception response

---
+++ src/main/java/br/com/apibachintegracao/service/exceptions/BadRequestException.java
@@ -0,0 +1,31 @@
+package br.com.apibachintegracao.service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private final int statusCode;
+	
+	public BadRequestException (String message, int statusCode) {
+		super(message);
+		this.statusCode = statusCode;
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+	public BadRequestException(String message) {
+		super(message);
+		this.statusCode = 400;
+	}
+	
+	
+
+}
